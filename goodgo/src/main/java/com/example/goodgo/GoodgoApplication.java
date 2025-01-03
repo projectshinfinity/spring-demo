aab8a629e33b4742cee63ada043b2dae0fc41ab6
EC2 Default User <ec2-user@ip-172-31-43-142.ap-south-1.compute.internal>
2025-01-03 06:18:29 +0000

    Update spring-demo repository

---
+++ goodgo/src/main/java/com/example/goodgo/GoodgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodgoApplication.class, args);
+	}
+
+}
---
+++ goodgo/src/test/java/com/example/goodgo/GoodgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
