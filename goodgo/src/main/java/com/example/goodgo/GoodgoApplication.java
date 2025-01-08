9bee82821cf0ef9cef7169aaa3a3c4eb7fa28a3c
projectshinfinity <project@hinfinitysolutions.com>
2025-01-08 12:08:07 +0530

    Update GoodgoApplication.java

--- goodgo/src/main/java/com/example/goodgo/GoodgoApplication.java
+++ goodgo/src/main/java/com/example/goodgo/GoodgoApplication.java
@@ -1,13 +1,19 @@
-package com.example.goodgo;
+package com.example.goodgo.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-public class GoodgoApplication {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoodgoApplication.class, args);
-	}
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow CORS for all endpoints
+                .allowedOrigins("http://13.234.35.193:4200")  // Allow requests from your Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Specify allowed HTTP methods
+                .allowedHeaders("*")  // Allow any headers
+                .allowCredentials(true);  // Allow credentials like cookies or authentication
+    }
 }
